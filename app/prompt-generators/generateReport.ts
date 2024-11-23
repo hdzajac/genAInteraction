6d112a5bded884bb62160a094718ebc7bab1484c
Jorge Ribeiro <jorge@jribeiro.org>
2024-11-23 16:31:22 +0000

    highlight input data on report

--- app/prompt-generators/generateReport.ts
+++ app/prompt-generators/generateReport.ts
@@ -1,9 +1,9 @@
+import { zodResponseFormat } from 'openai/helpers/zod'
 import { z } from 'zod'
 
-import { EvaluationReport } from '@/store/types'
-import { openai } from '@/openai'
-import { zodResponseFormat } from 'openai/helpers/zod'
 import { EvaluationLabels, SectionTypes } from '@/constants'
+import { openai } from '@/openai'
+import { EvaluationReport } from '@/store/types'
 
 export const sectionInfo = [
   {
@@ -73,13 +73,21 @@ export default async function ({ evaluation, sections }: Props) {
     return testingMode()
   }
 
+  const evaluationPrompt = (Object.keys(evaluation) as Array<keyof EvaluationReport>)
+    .filter((key) => evaluation[key] !== '')
+    .map((key) => {
+      return `
+    - ${EvaluationLabels[key]}: ${evaluation[key]}`
+    })
+
   const sectionsPrompt = sections
     .map((section) => {
       const sec = sectionInfo.find((info) => info.type === section)
 
       if (!sec) return ''
 
-      return `- ${sec.title} (${sec.type}): ${sec.description}`
+      return `
+      - ${sec.title} (${sec.type}): ${sec.description}`
     })
     .join('\n')
 
@@ -104,23 +112,15 @@ export default async function ({ evaluation, sections }: Props) {
       The report should include the following sections:
       ${sectionsPrompt}
 
-      You receive the following input, which is a text description of a patient's condition:
-        - Diagnosis: ${evaluation.diagnosis}
-        - Treatment: ${evaluation.treatment}
-        - Image Quality: ${evaluation.imageQuality}
-        - Visual features: ${evaluation.visualFeatures}
-        - Educational comments: ${evaluation.educationalComments}
+      You receive the following input, which describes the patient's condition:
+      ${evaluationPrompt}
 
-      Use that information when creating the report.
+      Use the patient condition when creating the report, and put the input data that is used in the report surrounded by the html tag <strong></strong>.
 
-      ## Here are some examples for each section:
-
-      ${examplesPrompt}
     `
-
   console.log('PROMPT', prompt)
 
-  const completion = await client.beta.chat.completions.parse({
+  const completion = await openai.beta.chat.completions.parse({
     model: 'gpt-4o-mini',
     messages: [{ role: 'system', content: prompt }],
     response_format: zodResponseFormat(ReportFormat, 'report'),
@@ -138,12 +138,12 @@ function testingMode() {
         {
           type: 'VISUAL_DESCRIPTION',
           content:
-            'The dermoscopic image presents a well-defined lesion measuring less than 6 mm with a uniform color, indicating a consistent morphology. The borders of the lesion are sharp, suggesting a demarcated border indicative of a benign nature. There are no irregularities in color or texture observed, which is consistent with a stable and non-suspicious appearance.',
+            'The dermoscopic image presents a well-defined lesion measuring less than  <strong>6 mm with a uniform color</strong>, indicating a consistent morphology. The borders of the lesion are sharp, suggesting a demarcated border indicative of a benign nature. There are no irregularities in color or texture observed, which is consistent with a stable and non-suspicious appearance.',
         },
         {
           type: 'ASSESSMENT',
           content:
-            'The clinical findings are suggestive of an Alm venus lesion, which appears benign in appearance based on its characteristics of uniform color, pronounced border, and size. Given the nature of the lesion and the recommendation of observation, I agree with the initial management plan.',
+            'The clinical findings are suggestive of an <strong>Alm venus</strong> lesion, which appears benign in appearance based on its characteristics of uniform color, pronounced border, and size. Given the nature of the lesion and the recommendation of observation, I agree with the initial management plan.',
         },
         {
           type: 'PRIMARY_PLAN',
@@ -159,16 +159,3 @@ function testingMode() {
     }, 200)
   })
 }
-
-// [
-//   {
-//     type: 'ASSESSMENT',
-//     content:
-//       'The patient has been diagnosed with an Alm venus. The lesion exhibits a uniform color, sharp borders, and is less than 6 mm in size. The image quality of the presentation is good, allowing for precise assessment. Given the described characteristics, the lesion appears benign, and the current treatment plan comprises observation.',
-//   },
-//   {
-//     type: 'PRIMARY_PLAN',
-//     content:
-//       'Continue with observation for the Alm venus lesion. Instruct the patient on the importance of using sunscreen daily to protect the skin from ultraviolet radiation, as this can help prevent any changes in the lesion and promote overall skin health. Recommend follow-up appointments at a six-month interval to reassess the lesion for any changes.',
-//   },
-// ]
