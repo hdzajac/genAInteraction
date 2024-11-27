bfb0bd931e6868b5a22e117d43cfde356beb8c02
Jorge Ribeiro <jorge@jribeiro.org>
2024-11-27 23:07:58 +0000

    clean up code

--- app/prompt-generators/generateReport.ts
+++ app/prompt-generators/generateReport.ts
@@ -4,53 +4,7 @@ import { z } from 'zod'
 import { EvaluationLabels, SectionTypes, SkinTypes } from '@/constants'
 import { openai } from '@/openai'
 import { EvaluationReport, Patient } from '@/store/types'
-
-export const sectionInfo = [
-  {
-    type: 'VISUAL_DESCRIPTION',
-    title: 'Objective visual description',
-    description: 'Your read of the image a description of objective and visual features',
-    examples: [
-      'The dermoscopic image reveals a 4-5mm irregular blue papule. There are shiny white structures present. The blue color is not homogeneous, and the presence of shiny white structures would not be typical for a blue nevus',
-    ],
-  },
-  {
-    type: 'ASSESSMENT',
-    title: 'Assessment',
-    description:
-      'Your first suggestion for immediate next steps (treatment, patient care, dismissal).',
-    examples: [
-      "The clinical presentation is consistent with rosacea, a chronic inflammatory skin condition. The patient's symptoms, including facial erythema, flushing, and the presence of papules and pustules, are consistent with a diagnosis of papulopustular rosacea",
-    ],
-  },
-  {
-    type: 'PRIMARY_PLAN',
-    title: 'Primary plan',
-    description:
-      'The recommended treatment plan, including any necessary follow-up appointments or instructions',
-    examples: [
-      'I would recommend topical treatment with metronidazole 0.75% gel 1-2 times. daily for 3-4 months, can be extended in case of positive effect and relapse. In the event of a lack of effect, consider starting for the time being. in oral treatment with oracea (doxycycline 40 mg) once daily for 3 months. Recommend a daily mild cleanser and a broad-spectrum sunscreen (SPF 30 or higher) to protect against UV-induced flare-ups.',
-    ],
-  },
-  {
-    type: 'ALTERNATIVE_PLAN',
-    title: 'Alternative plan',
-    description:
-      'An alternative to your first suggestion, either because there are possible alternative treatment plans or because of the conditionality of the assessment.',
-    examples: [
-      'Of note, if the patient/parent feels this rash is consist with prior atopic dermatitis flares she has had, she might benefit from a non-steroid for the face and neck, such as tacrolimus oint 0.1%. ',
-    ],
-  },
-  {
-    type: 'FOLLOW_UP',
-    title: 'Follow-up',
-    description:
-      'Your suggestion of the next steps after the completion of the primary and alternative plans',
-    examples: [
-      'Should the above fail to lead to improvement in the next 4-6 weeks I recommend evaluation by a dermatologist face-to-face. ',
-    ],
-  },
-]
+import sectionsInfo from './helpers/sections-info'
 
 const ReportFormat = z.object({
   report: z.array(
@@ -68,7 +22,7 @@ type Props = {
 }
 
 export default async function ({ evaluation, patient, sections }: Props) {
-  console.log('PAYLOAD>>', patient)
+  console.log('PAYLOAD', sections)
 
   if (process.env.TESTING_MODE === 'true') {
     return testingMode()
@@ -83,7 +37,7 @@ export default async function ({ evaluation, patient, sections }: Props) {
 
   const sectionsPrompt = sections
     .map((section) => {
-      const sec = sectionInfo.find((info) => info.type === section)
+      const sec = sectionsInfo.find((info) => info.type === section)
 
       if (!sec) return ''
 
@@ -94,7 +48,7 @@ export default async function ({ evaluation, patient, sections }: Props) {
 
   const examplesPrompt = sections
     .map((section) => {
-      const sec = sectionInfo.find((info) => info.type === section)
+      const sec = sectionsInfo.find((info) => info.type === section)
 
       if (!sec) return ''
 
---
+++ app/prompt-generators/helpers/sections-info.ts
@@ -0,0 +1,46 @@
+export default [
+  {
+    type: 'VISUAL_DESCRIPTION',
+    title: 'Objective visual description',
+    description: 'Your read of the image a description of objective and visual features',
+    examples: [
+      'The dermoscopic image reveals a 4-5mm irregular blue papule. There are shiny white structures present. The blue color is not homogeneous, and the presence of shiny white structures would not be typical for a blue nevus',
+    ],
+  },
+  {
+    type: 'ASSESSMENT',
+    title: 'Assessment',
+    description:
+      'Your first suggestion for immediate next steps (treatment, patient care, dismissal).',
+    examples: [
+      "The clinical presentation is consistent with rosacea, a chronic inflammatory skin condition. The patient's symptoms, including facial erythema, flushing, and the presence of papules and pustules, are consistent with a diagnosis of papulopustular rosacea",
+    ],
+  },
+  {
+    type: 'PRIMARY_PLAN',
+    title: 'Primary plan',
+    description:
+      'The recommended treatment plan, including any necessary follow-up appointments or instructions',
+    examples: [
+      'I would recommend topical treatment with metronidazole 0.75% gel 1-2 times. daily for 3-4 months, can be extended in case of positive effect and relapse. In the event of a lack of effect, consider starting for the time being. in oral treatment with oracea (doxycycline 40 mg) once daily for 3 months. Recommend a daily mild cleanser and a broad-spectrum sunscreen (SPF 30 or higher) to protect against UV-induced flare-ups.',
+    ],
+  },
+  {
+    type: 'ALTERNATIVE_PLAN',
+    title: 'Alternative plan',
+    description:
+      'An alternative to your first suggestion, either because there are possible alternative treatment plans or because of the conditionality of the assessment.',
+    examples: [
+      'Of note, if the patient/parent feels this rash is consist with prior atopic dermatitis flares she has had, she might benefit from a non-steroid for the face and neck, such as tacrolimus oint 0.1%. ',
+    ],
+  },
+  {
+    type: 'FOLLOW_UP',
+    title: 'Follow-up',
+    description:
+      'Your suggestion of the next steps after the completion of the primary and alternative plans',
+    examples: [
+      'Should the above fail to lead to improvement in the next 4-6 weeks I recommend evaluation by a dermatologist face-to-face. ',
+    ],
+  },
+]
